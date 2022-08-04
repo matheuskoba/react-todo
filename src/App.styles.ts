ceac8b1a3778acfd71d419dd489af3f92ed66724
matheuskoba <matheuskoba2021@icloud.com>
2022-08-04 16:02:19 -0300

    first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #17181F;
+    min-height: 100vh;
+    padding-top: 60px;
+`;
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+    color: white;
+`;
+export const Header = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    text-align: center;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+`;
---
+++ src/components/AddArea/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    border: solid 1px #555;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+
+    .image{
+        margin-right: 5px;
+    }
+
+    input{
+        color: white;
+        border: 0px;
+        outline: 0;
+        background: transparent;
+        font-size: 18px;
+        flex: 1;
+        margin-left: 10px;
+    }
+`;
